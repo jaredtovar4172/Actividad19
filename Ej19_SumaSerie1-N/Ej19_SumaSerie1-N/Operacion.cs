37db033c39ee2c6669c2d2ed272ef33fe29e9beb
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-23 21:17:29 -0600

    Add files via upload

---
+++ Ej19_SumaSerie1-N/Ej19_SumaSerie1-N/Operacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej19_SumaSerie1_N
+{
+    class Operacion
+    {
+        public double AddSum(double n)
+        {
+            double a = 0;
+
+            for (double i = 1; i < n; i++)
+            {
+                if (i % 1 == 0)
+                {
+                    a += 1 / (i + 1); 
+                }
+        }
+            return a;
+        }
+}
+}
+
